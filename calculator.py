2900184046bea6941ab074e4ee5d133975eadeb6
donricardogosta <horvathdanii99@gmail.com>
2021-10-04 11:51:52 +0200

    Fix: calculate, ask for a number return value

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 def is_number(str):
     try:
-        num=int(str)
+        num = int(str)
         return True
     except ValueError:
         return False
@@ -15,16 +15,16 @@ def is_valid_operator(operator):
 
 
 def ask_for_a_number(force_valid_input):
-    while(True):
+    while(force_valid_input):
         answer = input("Please provide a number\n")
         if is_number(answer) == True:
-            return answer
+            return int(answer)
         else:
             input("This didn't look like a number, try again.")
 
 
 def ask_for_an_operator(force_valid_input):
-    while(True):
+    while(force_valid_input):
         answer = input("Please provide an operator (one of +, -, *, /)!\n")
         if is_valid_operator(answer) != False:
             return answer
@@ -32,21 +32,18 @@ def ask_for_an_operator(force_valid_input):
             input("Unknown operator, try again.")
 
 
-def calc(operator, a, b):
-    if operator == None or a == None or b == None:
-        return "Wrong input, please try again"
-    else:
-        if operator == "+":
-            return a+b
-        elif operator == "-":
-            return a-b
-        elif operator == "*":
-            return a*b
-        elif operator == "/":
-            if a == 0 or b == 0:
-                return None
-            else:
-                return a/b
+def calc(a, operator, b):
+    if operator == "+":
+        return a+b
+    elif operator == "-":
+        return a-b
+    elif operator == "*":
+        return a*b
+    elif operator == "/":
+        if a == 0 or b == 0:
+            return None
+        else:
+            return a/b
 
 
 def simple_calculator():
